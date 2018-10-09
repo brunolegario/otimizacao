9e27dc6ef8c3a297efc749ab3ee7013af8f94685
Bruno Olegario <bruno.younner@gmail.com>
2018-10-09 02:25:01 -0300

    convex hull done

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ REFS.map = $('#map');
 
 // 0==CLOSEST PATH, 1==GRAHAM_SCAN, 2==FIND_THE_WAY
 let MODE = 0;
-let canAdd = true;
 
+let canAdd = true;
 let originalPoints = [
     {x: -13.0, y: 0.5},
     {x: -10.5, y: -11.5},
@@ -32,38 +32,48 @@ let pointsCount = 0;
 
 $(document).ready(function() {
     $('#map-container').on('click', function(event) {
-        //console.log(event);
-        addPoint(event);
+        CreateClickPoint(event);
     });
 });
 
-function addPoint(ev) {
+function CreateClickPoint(ev) {
     if (MODE !== 0 || !canAdd) { return; }
 
     let clickX = ev.pageX - 15;
     let clickY = ev.pageY - 100;
 
-    let roundedX = parseFloat(clickX.toFixed(1));
-    let roundedY = parseFloat(clickX.toFixed(1));
+    AddPoint(clickX, clickY);
+}
+function PresetPoints(ev) {
+    ev.stopPropagation();
+
+    // Get SVG map size
+    let map = REFS.map[0].getBoundingClientRect();
+    let posX = map.width/ 30;
+    let posY = map.height/ 30;
+
+    // Reset points and convert coordinates no px
+    DeleteAllPoints();
+    for (let i = 0; i < originalPoints.length; i++) {
+        let pointX = (originalPoints[i].x + 15) * posX;
+        let pointY = map.height - ((originalPoints[i].y + 15) * posY);
+
+        AddPoint(pointX, pointY);
+    }
+}
+function AddPoint(x, y) {
+    let roundedX = parseFloat(x.toFixed(1));
+    let roundedY = parseFloat(y.toFixed(1));
     let point = {
         id: `point-${pointsCount}`,
         x: roundedX,
         y: roundedY
     };
     points.push(point);
-    let circle = drawCircle(clickX, clickY);
-    $('#map').append(circle);
-
-    // console.log('X:');
-    // console.log(clickX);
-    // console.log('Y:');
-    // console.log(clickY);
-
-
-    //console.log(event.originalEvent.clickY);
+    DrawCircle(roundedX, roundedY);
+    pointsCount++;
 }
-
-function drawCircle(posX, posY) {
+function DrawCircle(posX, posY) {
     let newPoint = document.createElementNS(SVG_URL, 'circle');
         newPoint.setAttributeNS(null, 'r', 3);
         newPoint.setAttributeNS(null, 'cx', posX);
@@ -73,127 +83,203 @@ function drawCircle(posX, posY) {
     $(newPoint).attr('id', `point-${pointsCount}`);
     newPoint.addEventListener('mouseover', function() {
         console.log($(newPoint).attr('id'));
-    });
-    pointsCount++;
-
-    return newPoint;
-}
-
-function presetPoints(ev) {
-    ev.stopPropagation();
-
-    // Get SVG map size
-    let map = REFS.map[0].getBoundingClientRect();
-    let posX = map.width/ 30;
-    let posY = map.height/ 30;
-
-    // Reset points and convert coordinates no px
-    if (points) { points = []; }
-    pointsCount = 0;
-    for (let i = 0; i < originalPoints.length; i++) {
-        let pointX = (originalPoints[i].x + 15) * posX;
-        let pointY = map.height - ((originalPoints[i].y + 15) * posY);
-
-        let roundedX = parseFloat(pointX.toFixed(1));
-        let roundedY = parseFloat(pointY.toFixed(1));
-
-        let point = {
-            id: `point-${pointsCount}`,
-            x: roundedX,
-            y: roundedY
-        };
-
-        points.push(point);
-
-        let circle = drawCircle(pointX, pointY);
-        $('#map').append(circle);
-    }
-
-    //console.log(points);
-    //console.log(originalPoints);
-    //console.log(pointsCount);
-}
-
-function findClosestPair() {
-    if (!points) { return; }
-
-    // Order by X
-    let orderedPoints = sortArrayByXandY(points);
-
-    console.log(divideAndConquer(0, orderedPoints.length));
-}
-
-function sortArrayByXandY(arr) {
-    arr.sort(function(a, b) {
-        if (a.x - b.x === 0) {
-            return a.y - b.y;
-        }
-        return a.x - b.x;
+        console.log($(newPoint).attr('cx'));
+        console.log($(newPoint).attr('cy'));
     });
 
-    return arr;
+    $('#map').append(newPoint);;
 }
 
-function sortArrayByY(arr) {
-    arr.sort(function(a, b) {
-        return a.y - b.y;
-    });
+function FindClosestPair() {
+    if (points.length === 0) { return; }
 
-    return arr;
+    // If there are any highlighted points
+    REFS.map.children('.active').removeClass('active');
+
+    // Order by X (resolve ties by Y)
+    let orderedPoints = SortArrayByXandY(points);
+    let deltaObj = DivideAndConquer(0, orderedPoints.length);
+
+    $(`#${deltaObj.id1}`).addClass('active');
+    $(`#${deltaObj.id2}`).addClass('active');
 }
-
-function divideAndConquer(start, end) {
-    let delta;
+function DivideAndConquer(start, end) {
+    let deltaObj;
 
     if (end - start <= 3) {
         // 2 or 3 points = BRUTE FORCE
-        if (end -start === 2) {
+        if (end - start === 2) {
             let a = points[start];
             let b = points[end - 1];
 
-            console.log('2 points');
-            console.log(a.id);
-            console.log(b.id);
-            console.log(dist(a, b));
-            return dist(a, b);
+            return Dist(a, b);
         }
         else {
             let a = points[start];
             let b = points[start + 1];
             let c = points[end - 1];
 
-            let ab = dist(a, b);
-            let ac = dist(a, c);
-            let bc = dist(b, c);
+            let ab = Dist(a, b);
+            let ac = Dist(a, c);
+            let bc = Dist(b, c);
 
-            let dmin = Math.min(ab, ac, bc);
+            let dmin = Math.min(ab.dist, ac.dist, bc.dist);
 
-            console.log('3 points');
-            if (dmin === ab) { console.log(ab); return ab; }
-            else if (dmin === ac) { console.log(ac); return ac; }
-            else { console.log(bc); return bc; }
+            if (dmin === ab.dist) { return ab; }
+            else if (dmin === ac.dist) { return ac; }
+            else { return bc; }
         }
     } else {
+        // Divide into smaller arrays
+        // and gets the smallest distance among them
         let m = parseInt((start + end) / 2);
-        console.log('start');
-        console.log(start);
-        console.log('end');
-        console.log(end);
-        console.log('middle');
-        console.log(m);
 
-        let dminL = divideAndConquer(start, m);
-        let dminR = divideAndConquer(m, end);
+        let dminL = DivideAndConquer(start, m);
+        let dminR = DivideAndConquer(m, end);
 
-        if (dminL < dminR) { delta = dminL; }
-        else { delta = dminR; }
+        if (dminL.dist < dminR.dist) { deltaObj = dminL; }
+        else { deltaObj = dminR; }
 
-        console.log('delta');
-        console.log(delta);
-        return delta;
+        // Check the middle section
+        // and compares it with the previous dmin
+        let mediumX = (points[m-1].x + points[m].x)/2;
+
+        let pointsLeft = [];
+        for (let i = start; i < m; i++) {
+            if (points[i].x > mediumX - deltaObj.dist) {
+                pointsLeft.push(points[i]);
+            }
+        }
+        let pointsRight = [];
+        for (let i = m; i < end; i++) {
+            if (points[i].x < mediumX + deltaObj.dist) {
+                pointsRight.push(points[i]);
+            }
+        }
+
+        for (let i = 0; i < pointsLeft.length; i++) {
+            for (let j = 0; j < pointsRight.length; j++) {
+                let distMiddle = Dist(pointsLeft[i], pointsRight[j]);
+                if (distMiddle.dist < deltaObj.dist) {
+                    deltaObj = distMiddle;
+                }
+            }
+        }
+
+        return deltaObj;
     }
 }
 
-function dist(p1, p2) {
-    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+function GrahamScan() {
+    if (points.length === 0) { return; }
+    let convexHull = [];
+
+    // 3 or less points = no need to/can't find the hull
+    if (points.length <= 3) {
+        return points;
+    }
+
+    // Create a copy of the array and find the point
+    // with the smaller y
+    let restOfPoints = points;
+    let smallestY = points[0];
+    let indexSmallestY = 0;
+    for (let i = 1; i < points.length; i++) {
+        if (points[i].y == smallestY.y) {
+            if (points[i].x < smallestY.x) {
+                smallestY = points[i];
+                indexSmallestY = i;
+            }
+        }
+        else if (points[i].y < smallestY.y) {
+            smallestY = points[i];
+            indexSmallestY = i;
+        }
+    }
+
+    // Removes that point from the new array
+    restOfPoints.splice(indexSmallestY, 1);
+
+    // Sets a 'graham angle' for each point
+    // The new array is ordered by this angle afterwards
+    for (let i = 0; i < restOfPoints.length; i++) {
+        // Get a new coord for the point based on the pivot
+        let tempX = restOfPoints[i].x - smallestY.x;
+        let tempY = restOfPoints[i].y - smallestY.y;
+        restOfPoints[i].grahamAngle = GetGrahamAngle(tempX, tempY);
+    }
+    restOfPoints.sort(function(a, b) {
+        return a.grahamAngle - b.grahamAngle;
+    });
+
+    // Build the convex hull
+    convexHull.push(smallestY);
+    convexHull.push(restOfPoints[0]);
+    for (let i = 1; i < restOfPoints.length; i++) {
+        let ccw = CrossProduct(convexHull[convexHull.length - 2],
+                                convexHull[convexHull.length - 1],
+                                restOfPoints[i]);
+
+        if (ccw === 0) {
+            convexHull.pop();
+            convexHull.push(restOfPoints[i]);
+        } else if (ccw > 0) {
+            convexHull.push(restOfPoints[i]);
+        } else {
+            do {
+                convexHull.pop();
+                ccw = CrossProduct(convexHull[convexHull.length - 2],
+                                    convexHull[convexHull.length - 1],
+                                    restOfPoints[i]);
+            } while(ccw <= 0 && convexHull.length > 2);
+
+            convexHull.push(restOfPoints[i]);
+        }
+    }
+
+    return convexHull;
+}
+
+function SortArrayByXandY(arr) {
+    arr.sort(function(a, b) {
+        return (a.x - b.x === 0) ? a.y - b.y : a.x - b.x;
+    });
+    return arr;
+}
+function SortArrayByYandX(arr) {
+    arr.sort(function(a, b) {
+        return (a.y - b.y === 0) ? a.x - b.x : a.y - b.y;
+    });
+    return arr;
+}
+function Dist(p1, p2) {
+    let dist = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+
+    let distObj = {
+        id1: p1.id,
+        id2: p2.id,
+        dist: dist,
+    }
+
+    return distObj;
+}
+function GetGrahamAngle(x, y) {
+    if (y === 0) {
+        return 0;
+    } else if (x > 0) {
+        return Math.atan(y / x);
+    } else if (x < 0) {
+        return Math.atan(-x / y) + Math.PI/2;
+    } else {
+        return Math.PI/2;
+    }
+}
+function CrossProduct(p1, p2, p3) {
+    return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+}
+
+function DeleteAllPoints() {
+    points = [];
+    REFS.map.empty();
+    pointsCount = 0;
 }
